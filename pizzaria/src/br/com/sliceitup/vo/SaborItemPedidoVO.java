ede23f7d457f5eccb8bedeb66e5701fe1e1d7ac6
powerbeacht <geovan.girardi@gmail.com>
2016-09-03 20:51:46 -0300

    implementação de atributos e gerado get/set

--- pizzaria/src/br/com/sliceitup/vo/SaborItemPedidoVO.java
+++ pizzaria/src/br/com/sliceitup/vo/SaborItemPedidoVO.java
@@ -2,4 +2,32 @@ package br.com.sliceitup.vo;
 
 public class SaborItemPedidoVO {
 
+	private Integer id;
+	private SaborVO sabor;
+	private PedidoItemVO itemPedido;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public SaborVO getSabor() {
+		return sabor;
+	}
+
+	public void setSabor(SaborVO sabor) {
+		this.sabor = sabor;
+	}
+
+	public PedidoItemVO getItemPedido() {
+		return itemPedido;
+	}
+
+	public void setItemPedido(PedidoItemVO itemPedido) {
+		this.itemPedido = itemPedido;
+	}
+
 }
