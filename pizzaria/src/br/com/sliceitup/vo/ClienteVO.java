99d6c18e342f8154a4bd14cbb536bcb16fd43bf8
AllanHorst <allanhorstwillig@gmail.com>
2016-09-07 15:30:06 -0300

    Adicionado anotações hibernate e configurado hibernate.cfg

--- pizzaria/src/br/com/sliceitup/vo/ClienteVO.java
+++ pizzaria/src/br/com/sliceitup/vo/ClienteVO.java
@@ -1,10 +1,28 @@
 package br.com.sliceitup.vo;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cliente")
 public class ClienteVO {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
 	private Integer id;
+
+	@Column(name = "nome")
 	private String nome;
+
+	@Column(name = "telefone")
 	private String telefone;
+
+	@Column(name = "endereco")
 	private String endereco;
 
 	public Integer getId() {
--- pizzaria/src/br/com/sliceitup/vo/SaborVO.java
+++ pizzaria/src/br/com/sliceitup/vo/SaborVO.java
@@ -1,8 +1,22 @@
 package br.com.sliceitup.vo;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "sabor")
 public class SaborVO {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
 	private Integer id;
+
+	@Column(name = "descricao")
 	private String descricao;
 
 	public Integer getId() {
